befc65b3c1ce0b2591b54b9d8ae23d7b240e243a
Murray Lynch <mdlynch37@gmail.com>
2017-06-27 20:08:50 -0400

    Results of spot-checks from Implementation.ipynb

---
+++ Executable/TPOT/tpot_basic6feat_bench_light.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.tree import DecisionTreeClassifier
+from tpot.builtins import StackingEstimator
+
+# NOTE: Make sure that the class is labeled 'class' in the data file
+tpot_data = np.recfromcsv('PATH/TO/DATA/FILE', delimiter='COLUMN_SEPARATOR', dtype=np.float64)
+features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpot_data.dtype.names.index('class'), axis=1)
+training_features, testing_features, training_target, testing_target = \
+    train_test_split(features, tpot_data['class'], random_state=42)
+
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=DecisionTreeClassifier(max_depth=10, min_samples_leaf=19, min_samples_split=17)),
+    DecisionTreeClassifier(max_depth=4, min_samples_leaf=3, min_samples_split=6)
+)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
