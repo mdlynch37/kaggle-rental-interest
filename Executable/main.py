b602004d39d34152868e08a6837ae858f3ede9e1
Murray Lynch <mdlynch37@gmail.com>
2017-07-06 01:26:07 -0400

    Testing and main.py add to_min_secs and small fixes

--- Executable/main.py
+++ Executable/main.py
@@ -4,50 +4,75 @@ import pandas as pd
 
 SEED = 42
 
+
+def to_min_secs(seconds):
+    """Returns tuple of min, seconds."""
+    seconds = round(seconds)
+    return seconds//60, seconds % 60
+
+
 def read_rental_interest(fp, frac=None, random_state=None,
                          encode_labels=True):
-    """Reads Two Sigma Connect: Rental Listing Inquiries json."""
+    """Reads RentalHop listing json or .pkl data file.
 
+    Parameters
+    ----------
+    fp : str
+    frac : float, default None
+        Specficies size of stratified sample of read dataset.
+        Ignored for for .pkl files.
+    random_state : int, default None
+        Random seed for stratified sample.
+        Ignored for for .pkl files.
+    encode_labels : bool, default True
+        If True, encode 'high', 'medium' and 'low' as 3, 2, 1 resp.
+        Ignored for for .pkl files.
+
+    Returns
+    -------
+    df : pandas DataFrame
+    """
     if fp.split('.')[-1] == 'pkl':
-        return load_pickle(fp)
-
-    df=(pd.read_json(fp)
-        .set_index('listing_id')
-    )
-    df.created = pd.to_datetime(df.created)
-
-    # encode for automatic ordering when plotting
-    if True and 'interest_level' in df.columns:
-        df.interest_level = df.interest_level.replace(
-            ['low', 'medium', 'high'], [1, 2, 3]
+        df = load_pickle(fp)
+    else:
+        df=(pd.read_json(fp)
+            .set_index('listing_id')
         )
-    # stratified sample
-    if frac is not None:
-        df = (df.groupby('interest_level')
-             .apply(lambda x:x.sample(frac=frac,random_state=random_state))
-        )
+        df.created = pd.to_datetime(df.created)
+
+        # encode for automatic ordering when plotting
+        if encode_labels and 'interest_level' in df.columns:
+            df.interest_level = df.interest_level.replace(
+                ['low', 'medium', 'high'], [1, 2, 3]
+            )
+        # stratified sample
+        if frac is not None:
+            df = (df.groupby('interest_level')
+                    .apply(lambda x:x.sample(frac=frac,random_state=random_state))
+            )
 
     return df
 
 
-def save_submission(preds, fp):
+def save_submission(preds, index, fp):
     """Ensures correct format and class ordering."""
     submit = (pd.DataFrame(preds,
-                           columns=['high', 'low', 'medium'],
-                           index=X_sub.index)
-              .loc[:, ['high', 'medium', 'low']])
+                           columns=['low', 'medium', 'high'],
+                           index=index)
+              .loc[:, ['high', 'medium', 'low']]  # right order
+    )
 
     # ensure label ordering correct
     assert submit.high.sum() < submit.medium.sum() < submit.low.sum(), (
-        'Interest level probpip abilities are mismatched')
+        'Interest level probabilities are mismatched')
 
     submit.to_csv(fp)
 
-    return submit
 
 def dump_pickle(obj, fp):
     with open(fp, 'wb') as f:
         pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+
 
 def load_pickle(fp):
     with open(fp, 'rb') as f:
